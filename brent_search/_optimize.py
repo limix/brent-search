78bd196cf875efae57db3b141e52392f342d693c
Danilo Horta <danilo.horta@gmail.com>
2016-10-25 10:33:53 +0100

    bump version

--- brent_search/_optimize.py
+++ brent_search/_optimize.py
@@ -16,12 +16,8 @@ def minimize(f, x0=None, x1=None, a=-inf, b=+inf, gfactor=2,
     func.nfev = 0
 
 
-    r, ecode = bracket(func, x0=x0, x1=x1, a=a, b=b,
-                                            gfactor=gfactor, rtol=rtol,
-                                            atol=atol, maxiter=maxiter)
-
-
-
+    r, ecode = bracket(func, x0=x0, x1=x1, a=a, b=b, gfactor=gfactor, rtol=rtol,
+                       atol=atol, maxiter=maxiter)
 
     x0, x1, x2, f0, f1, f2 = r[0], r[1], r[2], r[3], r[4], r[5]
     x0, f0 = brent(func, x0, x2, f0, f2, x1, f1, rtol, atol, maxiter)[0:2]
