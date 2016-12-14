fb71142afb507d3ce454b20477df00c2d82da557
Danilo Horta <danilo.horta@gmail.com>
2016-12-14 16:28:25 +0000

    dev1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def setup_package():
 
     metadata = dict(
         name='brent-search',
-        version='1.0.17.dev0',
+        version='1.0.17.dev1',
         maintainer="Danilo Horta",
         maintainer_email="horta@ebi.ac.uk",
         description="Brent's method for univariate function optimization.",
