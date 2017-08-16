b5fbd86385e0e4c515948488c20f1c9cbeeca6ce
Danilo Horta <danilo.horta@gmail.com>
2017-08-16 19:18:30 +0100

    vamos la

--- brent_search/__init__.py
+++ brent_search/__init__.py
@@ -1,24 +1,9 @@
+from ._test import test
+from ._version import __version__, version_info
 from .bracket import bracket
 from .brent import brent
 from .optimize import minimize
 
-
-def test():
-    import os
-    p = __import__('brent_search').__path__[0]
-    src_path = os.path.abspath(p)
-    old_path = os.getcwd()
-    os.chdir(src_path)
-
-    try:
-        return_code = __import__('pytest').main(['-q', '--doctest-modules'])
-    finally:
-        os.chdir(old_path)
-
-    if return_code == 0:
-        print("Congratulations. All tests have passed!")
-
-    return return_code
-
-
-__all__ = ["test", "bracket", "brent", "minimize"]
+__all__ = [
+    "__version__", "version_info", "test", "bracket", "brent", "minimize"
+]
