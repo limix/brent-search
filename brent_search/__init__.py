d884d1938b827ea5a0df927c9ea66a5ce0859935
Danilo Horta <danilo.horta@gmail.com>
2018-01-21 00:37:35 +0000

    setup

--- brent_search/__init__.py
+++ brent_search/__init__.py
@@ -1,14 +1,8 @@
-from ._test import test
+from .testit import test
 from .bracket import bracket
 from .brent import brent
 from .optimize import minimize
 
-__name__ = "brent-search"
-__version__ = "1.0.29"
-__author__ = "Danilo Horta"
-__author_email__ = "horta@ebi.ac.uk"
+__version__ = "1.0.30"
 
-__all__ = [
-    "__name__", "__version__", "__author__", "__author_email__", "test",
-    "bracket", "brent", "minimize"
-]
+__all__ = ["__version__", "test", "bracket", "brent", "minimize"]
