2188cdc0fab20027f9fc5c5c8c869278a98db83c
Danilo Horta <danilo.horta@gmail.com>
2018-01-24 14:19:12 +0000

    testit

--- brent_search/testit.py
+++ brent_search/testit.py
@@ -1,35 +1,21 @@
-from os import chdir, getcwd
-from os.path import abspath, basename, dirname, realpath, join
-
-
 def test(verbose=True):
     r"""Run tests to verify this package's integrity.
 
     Parameters
     ----------
     verbose : bool
-    ``True`` to show diagnostic. Defaults to ``True``.
+        ``True`` to show diagnostic. Defaults to ``True``.
 
     Returns
     -------
     int
-    Exit code: ``0`` for success.
+        Exit code: ``0`` for success.
     """
-
-    pkgname = basename(dirname(realpath(__file__)))
-
-    p = __import__(pkgname).__path__[0]
-    src_path = abspath(p)
-    old_path = getcwd()
-    chdir(src_path)
 
     args = ['--doctest-modules', '-x', '--pep8']
     if not verbose:
-        args += ['-q']
+        args += ['--quiet']
 
-    try:
-        return_code = __import__('pytest').main(args)
-    finally:
-        chdir(old_path)
+    args += ['--pyargs', __name__.split('.')[0]]
 
-    return return_code
+    return __import__('pytest').main(args)
