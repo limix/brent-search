1346b3f5bc1a4dc3694902c4e5f910b0d70bf9ea
Danilo Horta <danilo.horta@gmail.com>
2018-01-25 10:44:00 +0000

    Include version, properly test

--- brent_search/__init__.py
+++ brent_search/__init__.py
@@ -1,8 +1,8 @@
-from .testit import test
 from .bracket import bracket
 from .brent import brent
 from .optimize import minimize
+from .testit import test
 
-__version__ = "1.0.30"
+__version__ = "1.0.31"
 
 __all__ = ["__version__", "test", "bracket", "brent", "minimize"]
