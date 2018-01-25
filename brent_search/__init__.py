6437332972717a608896a3fdfca844debe5db8e2
Danilo Horta <danilo.horta@gmail.com>
2018-01-25 16:34:25 +0000

    1.0.32, readthedocs

--- brent_search/__init__.py
+++ brent_search/__init__.py
@@ -3,6 +3,6 @@ from .brent import brent
 from .optimize import minimize
 from .testit import test
 
-__version__ = "1.0.31"
+__version__ = "1.0.32"
 
 __all__ = ["__version__", "test", "bracket", "brent", "minimize"]
